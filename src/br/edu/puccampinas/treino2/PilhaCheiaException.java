c39ffb22f28d3ac7ac9fc62749bd079f50c46b22
Aleph Santos Oliveira <aleph.oliveira@assertivasolucoes.com.br>
2019-04-01 07:49:48 -0300

    Add pacote da segunda lista de treino

---
+++ src/br/edu/puccampinas/treino2/PilhaCheiaException.java
@@ -0,0 +1,10 @@
+package br.edu.puccampinas.treino2;
+
+public class PilhaCheiaException extends Exception {
+
+  private static final long serialVersionUID = 1L;
+
+  public PilhaCheiaException(String message) {
+    super(message);
+  }
+}
---
+++ src/br/edu/puccampinas/treino2/PilhaVaziaException.java
@@ -0,0 +1,11 @@
+package br.edu.puccampinas.treino2;
+
+public class PilhaVaziaException extends Exception {
+
+  private static final long serialVersionUID = 1L;
+
+  public PilhaVaziaException(String message) {
+    super(message);
+  }
+
+}
